bc69805a0dc50656a87bda7072af1e45fe81ff5c
LUFT-AQUILA <mail@luftaquila.io>
2023-09-07 11:45:56 +0900

    fix: build project on STM32CubeIDE

--- device/TMA-1/Core/Src/main.c
+++ device/TMA-1/Core/Src/main.c
@@ -58,7 +58,10 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+__attribute__((weak)) void _close(void){}
+__attribute__((weak)) void _lseek(void){}
+__attribute__((weak)) void _read(void){}
+__attribute__((weak)) void _write(void){}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
