04e4c065b4835c5915e70d3f5f50a3a6a360d9dd
LUFT-AQUILA <mail@luftaquila.io>
2023-09-20 15:25:12 +0900

    add: build_config.json manipulation at config tool

--- builder/main.py
+++ builder/main.py
@@ -1,8 +1,6 @@
+import json
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDRectangleFlatButton
 
 class MainApp(MDApp):
     def __init__(self, **kwargs):
@@ -15,13 +13,42 @@ class MainApp(MDApp):
         return self.screen
 
     def on_start(self, **kwargs):
-        self.screen.ids.enable_telemetry
+        with open("build_config.json", "r") as file:
+            build_config = json.load(file)
+
+            self.screen.ids.telemetry.state = 'down' if build_config["STM32"]["output"]["telemetry"] else 'normal'
+            self.screen.ids.serial.state    = 'down' if build_config["STM32"]["output"]["serial"] else 'normal'
+
+            self.screen.ids.can.state           = 'down' if build_config["STM32"]["sensor"]["can"] else 'normal'
+            self.screen.ids.digital.state       = 'down' if build_config["STM32"]["sensor"]["digital"] else 'normal'
+            self.screen.ids.analog.state        = 'down' if build_config["STM32"]["sensor"]["analog"] else 'normal'
+            self.screen.ids.pulse.state         = 'down' if build_config["STM32"]["sensor"]["pulse"] else 'normal'
+            self.screen.ids.accelerometer.state = 'down' if build_config["STM32"]["sensor"]["accelerometer"] else 'normal'
+            self.screen.ids.gps.state           = 'down' if build_config["STM32"]["sensor"]["gps"] else 'normal'
+
+            self.screen.ids.debug_mode.state    = 'down' if build_config["STM32"]["debug"]["debug_mode"] else 'normal'
+            self.screen.ids.release_build.state = 'down' if build_config["STM32"]["debug"]["release_build"] else 'normal'
 
     def on_checkbox_active(self, checkbox, value):
-        if value:
-            print('The checkbox', checkbox, 'is active')
-        else:
-            print('The checkbox', checkbox, 'is inactive')
+        if checkbox in self.screen.ids.values():
+            id = list(self.screen.ids.keys())[list(self.screen.ids.values()).index(checkbox)]
+
+            build_config = ''
+            with open("build_config.json", "r+") as file:
+                build_config = json.load(file)
+
+                if id in build_config["STM32"]["output"]:
+                    build_config["STM32"]["output"][id] = value
+
+                elif id in build_config["STM32"]["sensor"]:
+                    build_config["STM32"]["sensor"][id] = value
+
+                elif id in build_config["STM32"]["debug"]:
+                    build_config["STM32"]["debug"][id] = value
+
+                file.seek(0)
+                json.dump(build_config, file, indent=2)
+                file.truncate()
 
 if __name__ == "__main__":
     MainApp().run()
